6e9d7bdfb7244313b84e33f95d9236fbd745c934
Sartr3 <153687520+Sartr3@users.noreply.github.com>
2024-04-25 09:28:46 +0000

    Initial commit

---
+++ docs/sapphire.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// Função para gerar a senha
+func generatePassword(length int) string {
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+,.?/:;{}[]~"
+	rand.Seed(time.Now().UnixNano())
+
+	password := make([]byte, length)
+	for i := range password {
+		password[i] = chars[rand.Intn(len(chars))]
+	}
+
+	return string(password)
+}
+
+// Manipulador para a rota "/generate-password"
+func passwordHandler(w http.ResponseWriter, r *http.Request) {
+	lstr := r.URL.Query().Get("length")
+	length, err := strconv.Atoi(lstr)
+	if err != nil {
+		http.Error(w, "Erro: o comprimento deve ser um número inteiro", http.StatusBadRequest)
+		return
+	}
+
+	// Gera a senha
+	password := generatePassword(length)
+
+	// Escreve a senh
+	w.Write([]byte("Senha gerada: " + password))
+}
+
+func main() {
+  // Mapeia o html
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "face.htm")
+	})
+
+	// Mapeia a rota
+	http.HandleFunc("/generate-password", passwordHandler)
+
+	// Inicia o servidor na porta 8080
+	fmt.Println("Servidor escutando em http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Erro ao iniciar o servidor: %s\n", err)
+	}
+}
